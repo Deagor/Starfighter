fe02cf15ba4cd29b3f1a54381df276472cf6e14f
Deagor <tiernanmartin@live.ie>
2014-10-20 12:40:43 +0100

    initial Commit

---
+++ Starfighter RnD/Starfighter RnD.cpp
@@ -0,0 +1,27 @@
+// Starfighter RnD.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#ifdef _DEBUG
+#pragma comment(lib,"sfml-graphics-d.lib")
+#pragma comment(lib,"sfml-audio-d.lib")
+#pragma comment(lib,"sfml-system-d.lib")
+#pragma comment(lib,"sfml-window-d.lib")
+#pragma comment(lib,"sfml-network-d.lib")
+#else
+#pragma comment(lib,"sfml-graphics.lib")
+#pragma comment(lib,"sfml-audio.lib")
+#pragma comment(lib,"sfml-system.lib")
+#pragma comment(lib,"sfml-window.lib")
+#pragma comment(lib,"sfml-network.lib")
+#endif
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glu32.lib")
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
